f406fb8aae32aae802f53ccfc3c63a90b23dd1cd
ngandinh1610js <ngandinh1610.js@gmail.com>
2021-12-14 15:34:13 +0700

    validate user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_one_attached :avatar
   validates :name, presence: true
+  validate :correct_image_type
   
+  private
+
+    def correct_image_type
+        if avatar.attached? && !avatar.content_type.in?(%w(image/jpeg image/-png))
+            errors.add(:avatar, "must has a JPEG or PNG.")
+        elsif avatar.attached? == false
+            errors.add(:avatar, "must required.")
+        end
+    end
 end
